add62f7926224cd5d1963705ca5a568b1b04210a
James Quintero <jquinte2@mail.sfsu.edu>
2018-02-23 16:25:58 -0800

    Added Milestones folder

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('about', views.about, name='about'),
-
 	path('about/alex', views.aboutAlex, name='aboutAlex'),
 	path('about/danielle', views.aboutDanielle, name='aboutDanielle'),
 	path('about/james', views.aboutJames, name='aboutJames'),
