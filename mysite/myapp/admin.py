4dbe717b22563050e06909c0bc6e5bd3cfdc65c2
withEspresso <dnunez@mail.sfsu.edu>
2018-03-13 15:42:11 -0700

    Created administrator access to models

--- mysite/myapp/admin.py
+++ mysite/myapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Users
+from .models import UserData
+from .models import Posts
+from .models import Locations
+from .models import Messages
 
 # Register your models here.
+admin.site.register(Users)
+admin.site.register(UserData)
+admin.site.register(Posts)
+admin.site.register(Locations)
+admin.site.register(Messages)
