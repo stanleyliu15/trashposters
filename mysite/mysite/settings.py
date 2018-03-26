37e32b65ac5b276786ffabf7979c8b2f9d0bdd4b
Danielle Nunez <dnunez@mail.sfsu.edu>
2018-03-25 18:20:15 -0700

    Fixed foreign key issue from last night, started abstracting templates.
    Now all users can make posts.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,7 +73,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
