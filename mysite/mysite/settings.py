66556f26172000b587a3a829ffe1005418f1fcce
Danielle Nunez <dnunez@mail.sfsu.edu>
2018-04-01 21:45:59 -0700

    Uploaded images now display on posts.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,8 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Note: This method of serving static files is only for development.
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'myapp/media')
 
 PROJECT_ROOT = 'mysite'
 
